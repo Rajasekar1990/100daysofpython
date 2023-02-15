fd80bf950c9bce3bb72b4db195dbda9cb537e0c6
rajasekar sundararajan <rajasekar.sundararajan@idfcfirstbank.com>
2023-02-16 00:17:29 +0530

    Project1 completed

---
+++ day1/project-BandNameGenerator/main.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator")
+cityname=input("what's name of the city you grew up in?\n")
+petname=input("what's your pet's name?\n")
+print("Your band name could be " + cityname + " " + petname)
