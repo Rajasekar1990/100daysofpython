8cb47b9c276069cef838cf7d23aaa9cf2697b9aa
rajasekar sundararajan <rajasekar.sundararajan@idfcfirstbank.com>
2023-02-17 21:28:07 +0530

    day3 Exercise3 completed

---
+++ day3/Ex3-LeapYear/main.py
@@ -0,0 +1,18 @@
+year=int(input("Which year do you want to check? "))
+
+flag=0
+if ((year % 4) == 0):
+    flag=1
+    if ((year % 100) == 0):
+        flag=1
+        if ((year % 400) == 0):
+            flag=1
+        else:
+            flag=0
+
+if (flag == 1):
+    print(f"{year} is a leap year")
+elif (flag == 0):
+    print(f"{year} is not a leap year")
+else:
+    print()
