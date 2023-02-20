09ec6fb84662bbc16e96a2eec43bc3218f626cdc
rajasekar sundararajan <rajasekar.sundararajan@idfcfirstbank.com>
2023-02-20 10:22:42 +0530

    day5 Exercise1 completed

---
+++ day5/Ex1-AvgHeight/main.py
@@ -0,0 +1,21 @@
+student_heights=input("Enter the list of student heights ").split()
+print(student_heights)
+#student_heights = [180, 124, 165, 173, 189, 169, 146]
+
+for i in range(0, len(student_heights)):
+    student_heights[i]=int(student_heights[i])
+    # print(student_heights[i])
+    # print(type(student_heights[i]))
+
+total=0
+count=0
+
+for j in student_heights:
+    total+=j
+    count+=1
+print(total)
+print(count)
+
+avg=round(total/count)
+
+print(f"Average height rounded to the nearest whole number = {avg}")
