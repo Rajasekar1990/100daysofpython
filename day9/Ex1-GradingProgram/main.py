439d8242eb8dcd5baa778def4296534327d3c6bb
rajasekar sundararajan <rajasekar.sundararajan@idfcfirstbank.com>
2023-02-28 12:08:38 +0530

    day9 Exercise1 completed

---
+++ day9/Ex1-GradingProgram/main.py
@@ -0,0 +1,23 @@
+#Welcome to Grading Program
+student_scores = {
+  "Harry": 81,
+  "Ron": 78,
+  "Hermione": 99, 
+  "Draco": 74,
+  "Neville": 62,
+}
+
+student_grades={}
+
+for student_name in student_scores:
+    score=int(student_scores[student_name])
+    if(score > 90):
+       student_grades[student_name]="Outstanding" 
+    elif(score > 80):
+       student_grades[student_name]="Exceeds Expectations" 
+    elif(score > 70):
+       student_grades[student_name]="Acceptable"
+    else:
+       student_grades[student_name]="Fail"
+    
+print(student_grades)
