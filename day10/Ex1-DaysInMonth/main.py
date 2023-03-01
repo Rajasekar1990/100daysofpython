bf554a3d286dc3e1b6006857ba932190a71ebd4a
rajasekar sundararajan <rajasekar.sundararajan@idfcfirstbank.com>
2023-03-01 23:45:13 +0530

    day10 Exercise1 completed

---
+++ day10/Ex1-DaysInMonth/main.py
@@ -0,0 +1,26 @@
+#Welcome to Days in a Month
+def is_leap(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+#print(is_leap(year=int(input("Enter the year to check: "))))
+
+def days_in_month(year, month):
+    month_list=[31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if (is_leap(year) and month == 2):
+        return 29
+    else:
+        return month_list[month - 1]
+        
+year = int(input("Enter a year: "))
+month = int(input("Enter a month: "))
+days = days_in_month(year, month)
+print(days)
