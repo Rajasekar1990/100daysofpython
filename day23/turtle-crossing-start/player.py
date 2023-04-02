45b4ce363646ada5d690e55d3c201e7f80cd8242
rajasekar sundararajan <rajasekar.sundararajan@idfcfirstbank.com>
2023-04-02 12:19:01 +0530

    day23 turtle capstone project completed but no validated

--- day23/turtle-crossing-start/player.py
+++ day23/turtle-crossing-start/player.py
@@ -7,6 +7,7 @@ UP = 90
 LEFT = 270
 RIGHT = 0
 
+
 class Player(Turtle):
     def __init__(self):
         super().__init__()
@@ -14,32 +15,39 @@ class Player(Turtle):
         self.color("black")
         self.penup()
         self.reset_pos()
-        #self.heading(90)  # set the heading of turtle to 90 so that it moves upwards
+        # self.heading(90)  # set the heading of turtle to 90 so that it moves upwards
+
+    def set_turtle_90_deg(self):
+        self.heading(90)
 
     def move_up(self):
         # new_ycor = self.ycor() + MOVE_DISTANCE
         # self.goto(self.xcor(), new_ycor)
-        self.heading(90)
+
+        # self.heading(90)
+        self.set_turtle_90_deg()
         self.forward(MOVE_DISTANCE)
 
     # allows player to move left while crossing
-    def move_right(self):
-        if self.heading() != LEFT:
+    def move_left(self):
+        if self.heading() != RIGHT:
             self.heading(270)
             self.forward(MOVE_DISTANCE)
         else:
-            self.heading(90)
+            self.set_turtle_90_deg()
+            self.heading(270)
             self.forward(MOVE_DISTANCE)
 
     # allows player to move right while crossing
-    def move_left(self):
-        if self.heading() != RIGHT:
+    def move_right(self):
+        if self.heading() != LEFT:
             self.heading(0)
             self.forward(MOVE_DISTANCE)
         else:
-            self.heading(90)
+            self.set_turtle_90_deg()
             self.heading(0)
             self.forward(MOVE_DISTANCE)
+
     def reset_pos(self):
         self.goto(STARTING_POSITION)
 
